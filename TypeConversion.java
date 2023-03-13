b0b14cd07ee351031c92c64fa39bd31318c0d81c
Hee Seo Kim <hisounni@ewhain.net>
2023-03-13 13:04:35 +0900

    Update TypeConversion.java

--- TypeConversion.java
+++ TypeConversion.java
@@ -8,7 +8,7 @@ public class TypeConversion {
 		System.out.println(10/4);
 		System.out.println(10.0/4);
 		System.out.println((char)0x12340041);//16진수 아스키 코드로 char 타입으로 받음
-		System.out.println((byte)(b+i));
+        System.out.println((byte)(b+i));//오버 플로우 때문에 한 바퀴 돌아서 값이 저렇게 나왔다
 		System.out.println((int)2.9 + 1.8);
 		System.out.println((int)(2.9 + 1.8));
 		System.out.println((int)2.9 + (int)1.8);
